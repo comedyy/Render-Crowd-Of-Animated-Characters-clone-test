a62c79cd06096be5da18a7448e68b7f0d5ee7f55
张敦勇 <xxx>
2019-06-11 22:19:56 +0800

    init_commit

---
+++ Assets/Demo/SpawnManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// demo使用，分配大量的活动的角色
+/// </summary>
+public class SpawnManager : MonoBehaviour {
+
+    #region 字段
+
+    public GameObject spawnPrefab;
+    public int gridWidth;
+    public int gridHeight;
+
+    #endregion
+
+
+    #region 方法
+
+    GameObject RondomOne() {
+        return spawnPrefab;
+    }
+
+    void Start()
+    {
+        for(int i = 0; i < gridWidth; i++)
+        {
+            for(int j = 0; j < gridHeight; j++)
+            {
+                GameObject o =  Instantiate<GameObject>(RondomOne(), new Vector3(i * 2, 0, j * 2), Quaternion.identity);
+                o.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+                MeshRenderer r =  o.GetComponent<MeshRenderer>();
+                MaterialPropertyBlock mp = new MaterialPropertyBlock();
+                mp.SetFloat("_TextureIndex", Random.Range(0, 4));
+                r.SetPropertyBlock(mp);
+            }
+        }
+    }
+
+
+    #endregion
+
+}
---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class NewBehaviourScript
+{
+    [MenuItem("Tools/xxx")]
+    static void CreateItem() {
+        Object[] selections = Selection.objects;
+
+        Texture2DArray texture_array = new Texture2DArray(1024, 1024, 4, TextureFormat.RGB24, false);
+        for (int i = 0; i < selections.Length; i++)
+        {
+            Graphics.CopyTexture(selections[i] as Texture2D, 0, texture_array, i);
+        }
+        AssetDatabase.CreateAsset(texture_array, "Assets/xxx.asset");
+    }
+}
