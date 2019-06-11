65185a59f648c11d8f7b72af103139baca5701b9
张敦勇 <524547312@qq.com>
2019-06-12 01:20:23 +0800

    动画加起来。

---
+++ Assets/Demo/SpawnManager1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// demo使用，分配大量的活动的角色
+/// </summary>
+public class SpawnManager1 : MonoBehaviour {
+
+    #region 字段
+
+    public GameObject spawnPrefab;
+    public int gridWidth;
+    public int gridHeight;
+
+    #endregion
+
+
+    #region 方法
+
+    GameObject RondomOne() {
+        return spawnPrefab;
+    }
+
+    void Start()
+    {
+        for(int i = 0; i < gridWidth; i++)
+        {
+            for(int j = 0; j < gridHeight; j++)
+            {
+                Instantiate<GameObject>(spawnPrefab, new Vector3(i * 2, 0, j * 2), Quaternion.identity);
+            }
+        }
+    }
+
+
+    #endregion
+
+}
---
+++ Assets/Demo4/NewBehaviourScript1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class NewBehaviourScript1
+{
+    [MenuItem("Tools/yyy")]
+    static void CreateItem() {
+        Object[] selections = Selection.objects;
+        int width = 512;
+        int height = selections.ToList().Sum(m => (m as Texture2D).height);
+
+        Texture2D texture = new Texture2D(width, height, TextureFormat.RGBAHalf, false);
+        int current_height = 0;
+        for (int i = 0; i < selections.Length; i++)
+        {
+            Texture2D t = selections[i] as Texture2D;
+            texture.SetPixels(0, current_height, width, t.height, t.GetPixels());
+            current_height += t.height;
+            Debug.Log(current_height * 1.0f / height);
+        }
+        texture.Apply();
+        AssetDatabase.CreateAsset(texture, ("assets/demo4/x.asset"));
+    }
+}
